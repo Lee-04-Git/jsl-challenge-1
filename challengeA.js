855563c66c1ad924c2407ec26c16d74147926100
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-08-26 10:21:23 +0200

    completed challenge a

---
+++ challengeA.js
@@ -0,0 +1,14 @@
+// Challenge A
+/* This is the firstname of the user
+
+const user = 'John'
+
+/* This is the lastname of the user /* const surname = 'Smith'
+
+console.log(user; surname;)
+*/
+
+const user = 'John';
+const surname = 'Smith';
+
+console.log(user, surname);
